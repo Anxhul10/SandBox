d700805ff8cabb05d27c83846bc9f3d1bf4dfa55
Anxhul10 <anxhul10@gmail.com>
2025-01-29 16:28:57 +0530

    fixed crash on update route

--- todo_typeScript/server.ts
+++ todo_typeScript/server.ts
@@ -9,37 +9,49 @@ interface Obj{
     description:String,
     id:number
 }
-let todo : Array<Obj> = []// array of object
+let todo: Array<Obj> = []// array of object
+interface Obj{
+    title:String,
+    description:String,
+    id:number
+}
+
 let idGlobal : number = 0;
 app.get('/todo',(req:Request, res:Response)=>{
     if(todo.length == 0) {
         res.send({message:"todo is empty"});
     }
-    res.send(todo)
+    else{
+        res.send(todo)
+    }
 })
 
 // add todo
 app.post('/todo',(req:Request, res:Response)=>{
     const temp = req.body;
     temp.id = idGlobal++;
-    console.log(temp)
+
     
     todo.push(temp)
     res.send({message:"todo added!!"})
 })
 
 app.put('/todo/:Id', (req:Request, res:Response)=>{
-    const id = parseInt(req.params.Id);
-    const temp = req.body;
-    console.log(id);
-    for(let i = 0 ; i < todo.length ; i++){
-        if(todo[i].id == id){
-            temp.id = id;
-            todo.push(temp);
-            res.send({message:"todo added successfully", data:temp})
+    const strId = req.params.Id;
+    const id = parseInt(strId);
+    let temp = req.body;
+    if(id != null){
+        for(let i = 0 ; i < todo.length ; i++){
+            if(todo[i].id == id){
+                temp.id = id;
+                todo[i] = temp;
+                res.send({message:"todo added successfully", data:temp})
+            }
         }
     }
-    res.send({message:"check id "})
+    else{
+        res.send({message:"check id "})
+    }
 })
 
 app.listen((port), ()=>{
